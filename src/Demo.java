49d48a27906c7c73ba09055fa4c2d508cfd029c1
MiguelGoulao <MiguelGoulao@users.noreply.github.com>
2021-09-24 10:48:09 +0100

    Added a second line

--- src/Demo.java
+++ src/Demo.java
@@ -1,5 +1,6 @@
 public class Demo {
     public static void main(String[] args) {
         System.out.println("This is a demo project!");
+        System.out.println("Let's see if it works.");
     }
 }
